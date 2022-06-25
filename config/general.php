2db67766bdb2c3835fc8b956647a45ef40c7a2bb
Ronan <roleary81@gmail.com>
2022-06-25 16:11:39 +0200

    and set to allow admin changes

--- config/general.php
+++ config/general.php
@@ -24,7 +24,7 @@ return [
     'devMode' => $isDev,
 
     // Whether administrative changes should be allowed
-    'allowAdminChanges' => $isDev,
+    'allowAdminChanges' => true,
 
     // Whether crawlers should be allowed to index pages and following links
     'disallowRobots' => !$isProd,
