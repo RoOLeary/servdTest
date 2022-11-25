4f33489984a16511ff17fe161da9b70aa6b48e90
Ronan O'Leary <ronanoleary@Ronan-OLearys-MacBook-Pro.local>
2022-11-25 15:35:35 +0100

    target singles entry with slug

--- config/element-api.php
+++ config/element-api.php
@@ -168,7 +168,7 @@ return [
             \Craft::$app->response->headers->set('Access-Control-Allow-Origin', '*');
             return [
                 'elementType' => Entry::class,
-                'criteria' => ['section' => 'recipes'],
+                'criteria' => ['section' => 'articles', 'relatedTo' => [ 'targetElement' => $slug ] ],
                 'elementsPerPage' => 10,
                 'transformer' => function(Entry $entry) {
 
